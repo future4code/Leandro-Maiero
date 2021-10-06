1ec6de31615e01fc9cb117dda3919796f8aaaa7f
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2021-10-06 18:21:26 -0300

    exercicios strings-arrays

---
+++ modulo1/strings-arrays/strings-arrays/template_javascript/index.js
@@ -0,0 +1,92 @@
+//1 -
+//Undefined
+
+//Null
+
+//11
+//3
+
+//[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//19
+//9
+
+//2
+//SUBI NUM ÔNIBUS EM MIRROCOS 
+//27
+//_____________________________________
+
+
+//1
+const nomeDoUsuario = prompt("Qual o seu nome?")
+const emailDoUsuario = prompt("Qual o seu e-mail?")
+
+
+console.log (nomeDoUsuario)
+console.log (emailDoUsuario)
+console.log ("O E-mail", emailDoUsuario, "Foi cadastrado com sucesso, Seja bem vinda(o)!", nomeDoUsuario)
+
+//2
+//a)
+const comidaPreferida = ["Japa","Churrasco", "Bife Acebolado", "Strogonoff", "Macarrão com Queijo" ]
+console.log (comidaPreferida)
+
+// //b)
+console.log ("Essas são minhas comidas preferidas:")
+console.log (comidaPreferida[0])
+console.log (comidaPreferida[1])
+console.log (comidaPreferida[2])
+console.log (comidaPreferida[3])
+console.log (comidaPreferida[4])
+
+// //c)
+
+const comidaUsuario = prompt("Qual sua comida preferida?")
+comidaPreferida[1] = comidaUsuario
+console.log (comidaPreferida)
+
+//3
+
+//a)
+// const listaDeTarefas = [ ... ]
+// console.log (listaDeTarefas)
+//b)
+const afazeres = prompt("Me conte três tarefas que você precisa fazer?");
+console.log (afazeres)
+
+ 
+//c)
+
+const afazeresUsuario = [" 1 Banho" , "2 Dormir" , "3 Acordar"]
+console.log (afazeresUsuario)
+
+//d)
+
+const afazeresDois = prompt("Quais já fez?")
+console.log (afazeresDois) 
+
+
+//e)
+
+afazeresUsuario.splice(1,1)
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
