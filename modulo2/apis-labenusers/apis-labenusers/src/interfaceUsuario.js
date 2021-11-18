768832e7282363d525aaa05de903f4a65515a77d
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2021-11-17 21:33:24 -0300

    labenusers

---
+++ modulo2/apis-labenusers/apis-labenusers/src/interfaceUsuario.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+
+export const Interface = styled.div`
+text-align: center;
+background-image: linear-gradient(-20deg, #b721ff 0%, #21d4fd 100%);
+min-height: 45vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(12px + 3vmin);
+border: 7px solid black;
+border-radius: 4%;
+width: 30%;
+justify-content:center;
+margin-top: 5%;
+margin-left:2%;
+box-shadow: 7px 7px 21px 0px rgba(0, 0, 0, 0.75);
+
+`
+
+
+
+
+
+
+
+
+
+
+
