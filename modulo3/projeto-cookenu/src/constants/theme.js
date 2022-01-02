d08017779cd82c85b59d4945cc929f0513acccc4
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-01-02 17:42:30 -0300

    Projeto Cookenu

---
+++ modulo3/projeto-cookenu/src/constants/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { primaryColor, neutralColor } from "./colors";
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: primaryColor,
+            contrastText:"white",
+        },
+        text:{
+            primary: neutralColor
+        }
+        
+    },
+});
+
+export default theme
