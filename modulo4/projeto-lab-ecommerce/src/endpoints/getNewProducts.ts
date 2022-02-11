8593c3c4b0e039ba5442672ab7d88d7b04ae586e
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-02-10 23:22:39 -0300

    first commit

---
+++ modulo4/projeto-lab-ecommerce/src/endpoints/getNewProducts.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express"
+import { connection } from "../data/connection"
+
+
+// export async function getAllProducts(
+//    req: Request,
+//    res: Response
+// ): Promise<void> {
+//    try {
+//       const result = await 
+
+      
+
+
+//    } catch (error) {
+//       res.status(500).send("Internal server error")
+//    }
+// }
+
---
+++ modulo4/projeto-lab-ecommerce/src/types.ts
@@ -0,0 +1,15 @@
+export type products = {
+   id: string
+   title: string
+   description: string
+   userId: string
+   
+}
+
+export type user = {
+   id: string
+   name: string
+   email: string
+   password: string
+  
+}
