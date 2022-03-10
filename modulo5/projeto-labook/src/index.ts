ecae95580a1a618703c9c30cdade59237a54e5a6
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-03-10 19:39:57 -0300

    signup

--- modulo5/projeto-labook/src/index.ts
+++ modulo5/projeto-labook/src/index.ts
@@ -1,10 +1,7 @@
 import { app } from './app'
-import UserBusiness from './business/UserBusiness'
+
 import UserController from './controller/UserController'
-import UserData from './data/UserData2'
-import { Authenticator } from './services/Authenticator'
-import { HashManager } from './services/HashManager'
-import { IdGenerator } from './services/IdGenerator'
+
 
 const userController = new UserController()
 
