ff4947dde5e9a0347bf74842e353012b0ddce110
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-03-14 23:34:13 -0300

    Exercicios testes automatizados

---
+++ modulo5/testes-automatizados/src/index.ts
@@ -0,0 +1,17 @@
+export interface User {
+   name: string
+   balance: number
+}
+
+
+function performPurchase(user: User, value: number): User | undefined {
+   if (user.balance >= value) {
+       return {
+           ...user,
+           balance: user.balance - value
+       }
+   }
+   return undefined
+}
+
+export default performPurchase
