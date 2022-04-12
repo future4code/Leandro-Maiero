bb08db8bdbd0e97839f9d32d6d6d98df8f6ce839
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-04-12 18:44:28 -0300

    migration

--- modulo6/rodada-cases-semana4/src/data/migrations.ts
+++ modulo6/rodada-cases-semana4/src/data/migrations.ts
@@ -5,6 +5,17 @@ class CreateTable extends BaseDatabase {
     create = async () => {
         await BaseDatabase.connection.raw(`
           (
+            CREATE TABLE IF NOT EXISTS amaroProducts (
+              id VARCHAR(255) PRIMARY KEY,
+              name VARCHAR (255) NOT NULL
+              );
+
+              CREATE TABLE IF NOT EXISTS amaroTags (
+                productId VARCHAR(255),
+                FOREIGN KEY (productId) references amaroProducts(id),
+                name VARCHAR (255) NOT NULL 
+                );
+            
      
         `)
     }
